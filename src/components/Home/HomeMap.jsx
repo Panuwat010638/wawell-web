4853f5f19b069ebf09e85824b05a183713d9097b
Panuwat010638 <122358819+Panuwat010638@users.noreply.github.com>
2024-01-31 09:14:08 +0700

    add

--- src/components/Home/HomeMap.jsx
+++ src/components/Home/HomeMap.jsx
@@ -5,7 +5,7 @@ export default function HomeMap() {
     <div className='bg-[#fcfcfc]'>
         <div className='max-w-full'>
           <div className='flex justify-center items-center w-full h-[250px] sm:h-[300px] md:h-[350px] lg:h-[400px]'>
-            <iframe className='flex w-full h-full' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15506.046108070797!2d100.5239203!3d13.6874401!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a3007fa3d221%3A0x7e74257d04e7bed4!2sWawell%20Decor!5e0!3m2!1sen!2sth!4v1706609045037!5m2!1sen!2sth" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe className='flex w-full h-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.5115270176993!2d100.5239203!3d13.6874401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a3007fa3d221%3A0x7e74257d04e7bed4!2sWawell%20Decor!5e0!3m2!1sen!2sth!4v1706667143528!5m2!1sen!2sth" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
     </div>
